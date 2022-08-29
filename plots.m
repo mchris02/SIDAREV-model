efe0632cf784095f7edc6867fd9f611c2bf3743a
mchris02 <107786486+mchris02@users.noreply.github.com>
2022-08-29 17:47:45 +0300

    Delete plots.m

--- plots.m
+++
@@ -1,6 +0,0 @@
-function [x, y, z] = plots(dt, beta, gamma_i, gamma_d, gamma_a, ksi_i, ksi_d, mi, C_dth, c_1_a, pi_val,psi_val,s,z)
-
-x = linspace(-2,2,20);
-y = x';
-z = x .* exp(-x.^2 - y.^2);
-surf(z,y,z);
